28f74f1a92fb7adb7272d1ff6c11e79e26f7a4b8
Santo <santomonce@gmail.com>
2025-09-19 14:24:10 +0530

    code to find the largest among 3 using if else

---
+++ js-basics/random-codes-in-JS/largest-among-three.js
@@ -0,0 +1,11 @@
+let a = 1
+let b = 2
+let c = 3
+
+if (a>b && a>c){
+  console.log(a,"is the greatest");  
+}else if(b>a && b>c){
+  console.log(b,"is the greatest");  
+}else{
+  console.log(c,"is the greatest");  
+}
