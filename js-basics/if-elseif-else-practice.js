640d93582181cc0c35530a93a2d974fd63e96d46
Santo <santomonce@gmail.com>
2025-09-19 14:05:52 +0530

    improved version of if else, including elseif

---
+++ js-basics/if-elseif-else-practice.js
@@ -0,0 +1,18 @@
+let santo = 10;
+let haider = 40;
+let jayanth = 30;
+
+if(jayanth> 50){
+
+  console.log("We are rich");    
+
+}else if (((santo > 10) && (haider > 10)) || jayanth < 50){
+  
+  console.log("Jayanth is poor");    
+
+}else{
+
+   console.log("We are poor ");    
+
+}
+
