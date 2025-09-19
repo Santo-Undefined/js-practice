15640e798bf2ec9885185c4c470d2cc2ccf07960
Santo <santomonce@gmail.com>
2025-09-19 17:05:57 +0530

    code to find the day of the week using switch case

---
+++ js-basics/random-codes-in-JS/weekday-with-switchcase.js
@@ -0,0 +1,30 @@
+let day = "Monday"
+
+switch (day){
+  
+  case "Monday":
+    console.log("Its is Monday");
+    // break;
+  
+  case "Tuesday":
+    console.log("Its is Tuesday");
+    // break;
+
+  case "Wednesday":
+    console.log("Its is Wednesday");
+    // break;
+
+  case "Thursday":
+    console.log("Its is Thursday");
+    // break;
+  
+  case "Friday":
+    console.log("Its is Friday");
+    // break;
+  
+  default:
+    console.log("Which universe are you in BRO!!! ");
+    console.log("Check the spelling and re-run the code");
+
+}
+  
