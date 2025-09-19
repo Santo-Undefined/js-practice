ce1d1fbae13f8f937c805a1d7767cf59610f50a4
Santo <santomonce@gmail.com>
2025-09-19 14:27:48 +0530

    code to find the second largest number among 3 numbers using if else

---
+++ js-basics/random-codes-in-JS/second-largest-number-among-three.js
@@ -0,0 +1,11 @@
+let a = 95
+let b = 99
+let c = 97
+
+if (a>b && a<c){
+  console.log(a,"is the 2nd largest");  
+}else if(b>a && b<c){
+  console.log(b,"is the 2nd largest");  
+}else{
+  console.log(c,"is the 2nd largest");  
+}
