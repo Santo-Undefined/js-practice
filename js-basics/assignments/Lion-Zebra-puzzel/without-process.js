eb72f427ee717400f5c5531ed53694a1c869b644
Santo <santomonce@gmail.com>
2025-09-20 22:17:24 +0530

    Update  with proper comments
    Included proper comments and alignments done

--- js-basics/assignments/Lion-Zebra-puzzel/without-process.js
+++ js-basics/assignments/Lion-Zebra-puzzel/without-process.js
@@ -1,31 +1,45 @@
-const savanna = "L     Z"
+const testcase1 = "LZ";
+const testcase2 = "Z L";
+const testcase3 = "L     Z";
+const testcase4 = "L     L";
+const testcase5 = "Z   Z   Z";
+const testcase6 = "L  ZL Z";
+const testcase7 = "  LL  Z  Z";
 
-let animalDiversity = -1 ;
+const savanna = testcase7; //working condition
+
+let animalDiversity = -1;
 let animaldistance = savanna.length;
 
-for (let savannaPosition = savanna.length - 1; savannaPosition >= 0; savannaPosition-- ){
+// 
+for (let savannaPosition = savanna.length - 1; savannaPosition >= 0; savannaPosition--) {
 
-  if ( ((savanna[savannaPosition] === "L") || (savanna[savannaPosition] === "Z")) ) {
+  // checking if it is a animal or not
+  if (((savanna[savannaPosition] === "L") || (savanna[savannaPosition] === "Z"))) {
 
-    let animal1 = (savanna[savannaPosition] === "L")? "L" : "Z"
-    for(let savannaPositionInside = (savanna.length - 1); savannaPositionInside >=0 ; savannaPositionInside--){
-      
-      if ((savanna[savannaPositionInside] === "L") || (savanna[savannaPositionInside] === "Z")){
-        
-        if (animal1 !== savanna[savannaPositionInside]){
-          
-          let animalDiversityinside = savannaPosition - savannaPositionInside;
-          animalDiversityinside = (animalDiversityinside<0) ? (animalDiversityinside * -1) : animalDiversityinside;
-          animaldistance = (animalDiversityinside<animaldistance) ? animalDiversityinside : animaldistance;
-          // console.log( "Animal distance" , animaldistance);
+    const animal1 = (savanna[savannaPosition] === "L") ? "L" : "Z"; // finding which animal
+
+    // Finding the animals nearby and calculating the distance
+    for (let savannaPositionInside = (savanna.length - 1); savannaPositionInside >= 0; savannaPositionInside--) {
+
+      if ((savanna[savannaPositionInside] === "L") || (savanna[savannaPositionInside] === "Z")) {
+
+        // Checking if it is the opposite animal
+        if (animal1 !== savanna[savannaPositionInside]) {
+
+          let animalDiversityinside = savannaPosition - savannaPositionInside;  // calculating the distance
+          animalDiversityinside = (animalDiversityinside < 0) ? (animalDiversityinside * -1) : animalDiversityinside; // finding the shortest distance between the animals
+
+          animaldistance = (animalDiversityinside < animaldistance) ? animalDiversityinside : animaldistance;
 
         }
       }
     }
   }
 }
-// animaldistance = animaldistance - 1
-animaldistance = (animaldistance === savanna.length)? -1 : animaldistance - 1;
-console.log( "Final distance" , animaldistance);
 
-// console.log("Animal Diversity ", animalDiversity);
+animaldistance = (animaldistance === savanna.length) ? -1 : animaldistance - 1;
+console.log("Final distance", animaldistance);
+
+
+
