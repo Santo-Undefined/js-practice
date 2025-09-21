ab9ffcf952a2764dcd0fceea23686dda370108a4
Santo <santomonce@gmail.com>
2025-09-21 14:19:28 +0530

    Syntax and simple usage of ternary operator

---
+++ js-basics/JS-syntax-and-practice/JS-syntaxes/ternary-operator-syntax.js
@@ -0,0 +1,15 @@
+/* 
+Ternary operator:
+The ternary operator is used as a kind of simple 'if else' for only two SpeechRecognitionResultList
+ */
+
+
+const a = 10;
+const b = 20;
+//   syntax:
+const variable = (a > b) ? "A is bigger" : "B is bigger";
+//              conditon ⬆️  trueCondition   falseCondition
+//                 ternary operator
+
+console.log("The result is", variable);
+// expected result is "B is bigger"
