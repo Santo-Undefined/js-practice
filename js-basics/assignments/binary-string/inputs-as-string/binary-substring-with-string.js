e6f269d7ceff4e325712eef267a29f4f85e5fac9
Santo <santomonce@gmail.com>
2025-09-21 15:59:03 +0530

    Convert the number into Binary and into the binary string

--- js-basics/assignments/binary-string/inputs-as-string/binary-substring-with-string.js
+++ js-basics/assignments/binary-string/inputs-as-string/binary-substring-with-string.js
@@ -1,4 +1,6 @@
-const InputString = "3478";
+// here we tried to implement substring and the input number as a string
+
+const InputString = "10";
 const subString = "12";
 
 console.log("\nGiven number", InputString);
@@ -6,3 +8,24 @@ console.log("\nGiven number", InputString);
 // Making the given input string into a number
 let InputNumber = InputString % Infinity;
 console.log(InputNumber);
+
+// Making Binary of that number
+let InputBinary = 0;
+let InputBinaryMultiplier = 1;
+
+while (InputNumber >= 1) {
+  
+  const remainder = InputNumber % 2;
+  InputNumber = ( remainder === 0) ? InputNumber : InputNumber - 1; // modifying the number to be alway even number
+  InputNumber = InputNumber / 2;
+  
+  InputBinary = InputBinary + (remainder * InputBinaryMultiplier);  // Making the binary number
+  InputBinaryMultiplier = InputBinaryMultiplier * 10;
+
+  console.log( InputBinary);
+
+}
+
+// Making the input binary into a string
+const InputBinaryString = "" + InputBinary;
+console.log(InputBinaryString);
