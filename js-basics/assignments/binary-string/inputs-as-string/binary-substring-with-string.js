4a819221669c48b5ac2b206b25c62b70723e8333
Santo <santomonce@gmail.com>
2025-09-21 15:38:17 +0530

    Created a code that can convert the input string to number
    the input is a numerical string
    And i am converting it into a number

---
+++ js-basics/assignments/binary-string/inputs-as-string/binary-substring-with-string.js
@@ -0,0 +1,8 @@
+const InputString = "3478";
+const subString = "12";
+
+console.log("\nGiven number", InputString);
+
+// Making the given input string into a number
+let InputNumber = InputString % Infinity;
+console.log(InputNumber);
