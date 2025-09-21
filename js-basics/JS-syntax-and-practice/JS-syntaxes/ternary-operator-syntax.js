d8c6ed4b105d283cd0b378bb2d161c6080d234db
Santo <santomonce@gmail.com>
2025-09-21 14:36:11 +0530

    Added nested ternary example

--- js-basics/JS-syntax-and-practice/JS-syntaxes/ternary-operator-syntax.js
+++ js-basics/JS-syntax-and-practice/JS-syntaxes/ternary-operator-syntax.js
@@ -6,6 +6,7 @@ The ternary operator is used as a kind of simple 'if else' for only two SpeechRe
 
 const a = 10;
 const b = 20;
+const c = 30;
 //   syntax:
 const variable = (a > b) ? "A is bigger" : "B is bigger";
 //              conditon ⬆️  trueCondition   falseCondition
@@ -13,3 +14,8 @@ const variable = (a > b) ? "A is bigger" : "B is bigger";
 
 console.log("The result is", variable);
 // expected result is "B is bigger"
+
+
+// nested ternary example
+const variable2 = (a > b) ? ((a > c) ? "A is the largest number" : "A is a number") : "A is a number";
+console.log(variable2);
