8ea9d786320ec6d071adcb25b152d83dee1ac5cf
Santo <santomonce@gmail.com>
2025-09-27 19:55:03 +0530

    String spiltter
    It started off with the process, but in between I lost track
    So some test cases are missing according to the process

--- js-basics/assignments/string_splitter/string_splitter.js
+++ js-basics/assignments/string_splitter/string_splitter.js
@@ -1,5 +1,3 @@
-
-
 function isVowel(character) {
   const vowelString = "aeiou";
   for (let index = 0; index < vowelString.length; index++){
@@ -11,21 +9,24 @@ function isVowel(character) {
 }
 
 function stringSplitter(string) {
-  let spilttedString = "";
-  for (let index = 0; index < string.length; index++) {
-    const characterType = (isVowel(string[index])) ? "Vowel" : "Consonent";
-    console.log(string[index], "is", characterType);
+  let alternatingString = string[0];
+  let missMatchString = "";
 
-    if (string[index] === "c") {
-      spilttedString = spilttedString + ",";
-    }
+  for (let index = 1; index < string.length; index++) {
+    const currentCharacterType = isVowel(string[index]);
+    const lastCharacterType = isVowel(string[index -1]);
 
-    if (string[index] === "d") {
-      spilttedString = spilttedString + ",";
+    if (currentCharacterType !== lastCharacterType) {
+      alternatingString = alternatingString +string[index];
+    } else {
+      missMatchString = missMatchString + "," + string[index];
     }
-    spilttedString = spilttedString + string[index];
   }
-  return spilttedString;
+  
+  if (missMatchString.length === 0){
+    return alternatingString;
+  }
+  return alternatingString + missMatchString;
 }
 
 function composeResult(string, actual, expected) {
@@ -44,9 +45,14 @@ function testStrinSplitter(string, expected) {
 }
 
 function testAll() {
-  testStrinSplitter("a", "a");
-  testStrinSplitter("ab", "ab");
-  testStrinSplitter("abcd", "ab,c,d");
+  testStrinSplitter("abe", "abe");
+  testStrinSplitter("abcdefg", "abef,c,d,g");
+  testStrinSplitter("abeci", "abeci");
+  testStrinSplitter("apple", "ape,p,l");
+  testStrinSplitter("there", "tere,h");
+  testStrinSplitter("hello", "helo,l");
+  testStrinSplitter("abyss", "ab,y,s,s");
+  testStrinSplitter("this", "tis,h");
 }
 
 testAll();
