0707edadbce882579b364a05b2cff6a9933e1bfc
Santo <santomonce@gmail.com>
2025-10-22 21:23:25 +0530

    array sorting working
    tried to follow TDD
    if you have more suggestions for test cases let me know

--- js-basics/random-codes-in-JS/array_sorting.js
+++ js-basics/random-codes-in-JS/array_sorting.js
@@ -1,5 +1,35 @@
+/* contract
+    Inputs should be given as an array of numbers
+    Inputs shall not have nested arrays
+    All the input numbers should be whole numbers (negative numbers are accepted)
+*/
+function findSmallestElement (arr) {
+  let smallestElement =arr[0];
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] < smallestElement) {
+      smallestElement = arr[index];
+    }
+  }
+  return smallestElement;
+}
+
+function copyArr(arr) {
+  const arrCopy = [];
+  for (let index = 0; index < arr.length; index++) {
+    arrCopy.push(arr[index]);
+  }
+  return arrCopy;
+}
+
 function arraySorting(givenArr) {
- return [1,2,3];
+  const arrCopy = copyArr(givenArr);
+  const sortedArr = [];
+  while (sortedArr.length !== givenArr.length) {
+    const smallestElement = findSmallestElement(arrCopy);
+    sortedArr.push(smallestElement);
+    arrCopy.splice(arrCopy.indexOf(smallestElement), 1); //removing the smallest element
+  }
+  return sortedArr;
 }  
 
 function areEqual(arr1, arr2) {
@@ -36,8 +66,11 @@ function testArraySorting(description, givenArr, expectedArr){
 }
 
 function main() {
-  testArraySorting("dry run", [1,2,3], [1,2,3]);
-  // testArraySorting();
+  testArraySorting("simple sorted arrays", [1,2,3], [1,2,3]);
+  testArraySorting("simple mixed arrays", [1,4,3], [1,3,4]);
+  testArraySorting("empty arrays", [], []);
+  testArraySorting("negative numbers", [2,0,-12,-1], [-12,-1,0,2]);
+  testArraySorting("multiple similar elements", [2,4,1,2,2,3], [1,2,2,2,3,4]);
 }
 
 main();
