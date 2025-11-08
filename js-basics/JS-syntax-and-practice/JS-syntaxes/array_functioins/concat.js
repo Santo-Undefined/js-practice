cdb579283139d632b86e910af4f939782b889852
Santo K M <santokm@Santos-MacBook-Pro.local>
2025-11-08 14:59:20 +0530

    file made for .concate

---
+++ js-basics/JS-syntax-and-practice/JS-syntaxes/array_functioins/concat.js
@@ -0,0 +1,28 @@
+/* array.concat(<value>)
+   ---------------------
+  This function concatinates any element/value you provide 
+  to the end of the array
+  And increments the highest index value 
+
+  The provided implementation may be inaccurate for certain
+  inputs and are only meant to a simple visualization
+*/
+
+const array = [1,2,3,4,5]
+
+const findLastIndex = arr => arr.length - 1; //finds the last index of the array
+
+function concat(array, newElement) {
+  const newArray = array.slice();    //makes a copy of the array
+  const lastIndex = findLastIndex(newArray)
+  newArray[lastIndex + 1] = newElement;
+  return newArray
+}
+
+console.log("Self implemented function");
+console.log("Array before concantenating =", array);
+console.log("Array after concantenating 6 =", concat(array, 6));
+
+console.log("\nBuilt-in function");
+console.log("Array before concantenating =", array);
+console.log("Array after concantenating 6 =", array.concat(6));
