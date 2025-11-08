1be97cabd8c42aaabb44451d9a10feae063ccf07
Santo K M <santokm@Santos-MacBook-Pro.local>
2025-11-08 14:58:42 +0530

    Improved the .length description

--- js-basics/JS-syntax-and-practice/JS-syntaxes/array_functioins/length.js
+++ js-basics/JS-syntax-and-practice/JS-syntaxes/array_functioins/length.js
@@ -1,8 +1,15 @@
-// .length returns the length of the given array
-// Inside the compiler the working is very different, this is a very
-// simplified repersentation and not accurate
-// 
-// Actual the .length is returning +1 to the highest index
+/* array.length
+   ------------
+  This function returns the length of the provided array
+  
+  array.length returns the highest index + 1 (since index starts from 0)
+  The highest index value of array will be stored internally
+
+  The provided implementation may be inaccurate for certain
+  inputs and are only meant to a simple visualization
+
+  Note: .length does not count indexes as shown below 
+*/
 
 const array = [1,2,3,4,5]
 
@@ -14,5 +21,10 @@ function length(array) {
   return index
 }
 
-console.log("Self implemented function output", length(array));
-console.log("Built-in function output", array.length);
+console.log("Self implemented function");
+console.log("Array =", array);
+console.log("Array length =", length(array));
+
+console.log("\nBuilt-in function");
+console.log("Array =", array);
+console.log("Array length =", array.length);
