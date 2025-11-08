bd6842b430209509fbc70b0d471b6bdd5f139c68
Santo K M <santokm@Santos-MacBook-Pro.local>
2025-11-08 14:18:14 +0530

    A very simplified representation of array.length

---
+++ js-basics/JS-syntax-and-practice/JS-syntaxes/array_functioins/length.js
@@ -0,0 +1,17 @@
+// .length returns the length of the given array
+// Inside the compiler the working is very different, this is a very
+// simplified repersentation and not accurate
+// 
+// Actual the .length is returning +1 to the highest index
+
+const array = [1,2,3,4,5]
+
+function length(array) {
+  let index = 0;
+  while (array[index] !== undefined){
+    index++;
+  }
+  return index
+}
+
+console.log(length(array));
